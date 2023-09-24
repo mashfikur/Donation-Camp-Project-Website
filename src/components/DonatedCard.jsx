f20eff5ea0cbb919805fae5134912ab0b9adf70b
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-09-25 02:24:56 +0600

    donation card section style updated

--- src/components/DonatedCard.jsx
+++ src/components/DonatedCard.jsx
@@ -4,18 +4,32 @@ const DonatedCard = ({ donated }) => {
   return (
     <div>
       <div>
-        <div className={`card card-side bg-[] border-2`}>
-          <figure>
+        <div
+          className={`card card-side bg-[${donated.backgroundColor}] rounded-md`}
+        >
+          <div>
             <img
               src={donated.cardImage}
               alt="Movie"
+              className="h-full rounded-md"
             />
-          </figure>
+          </div>
           <div className="card-body">
-            <h2 className="card-title">New movie is released!</h2>
-            <p>Click the button to watch on Jetflix app.</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Watch</button>
+            <h3
+              className={`p-1 bg-[${donated.backgroundColor}] text-[${donated.textColor}] w-3/12 text-center rounded-md capitalize`}
+            >
+              {donated.category}
+            </h3>
+            <h2 className="font-semibold text-xl">{donated.title}</h2>
+            <p className={`text-[${donated.textColor}] font-semibold`}>
+              ${donated.donationPrice.toFixed(2)}
+            </p>
+            <div className="card-actions justify-start">
+              <button
+                className={`btn text-white capitalize bg-[${donated.textColor}] hover:bg-[${donated.textColor}]`}
+              >
+                View Details
+              </button>
             </div>
           </div>
         </div>
